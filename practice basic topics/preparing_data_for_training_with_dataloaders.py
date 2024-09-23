b5a3201620537db6a3b8f74a47800728aa1f5037
Shivam <coolsan929@gmail.com>
2024-09-23 20:07:23 +0530

    commit

--- practice basic topics/preparing_data_for_training_with_dataloaders.py
+++ practice basic topics/preparing_data_for_training_with_dataloaders.py
@@ -1,6 +1,4 @@
 from torch.utils.data import DataLoader
-import torch
-from torch.utils.data import Dataset
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
